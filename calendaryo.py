4cafe74baa4adf7cf7999ecfb618a5693eee2bcc
Marco Rougeth <marco@rougeth.com>
2019-09-15 19:43:23 +0100

    pybr19 updates

--- calendaryo.py
+++ calendaryo.py
@@ -17,6 +17,7 @@ client = discovery.build("calendar", "v3", credentials=credentials)
 # Create new one if not found.
 today = datetime.now()
 calendar_name = f"Python Brasil {today.year} - Grade"
+print(f"📆  {calendar_name}\n")
 
 for calendar in client.calendarList().list().execute()["items"]:
     if calendar["summary"] == calendar_name:
@@ -36,20 +37,26 @@ print("ACL created, id:", acl["id"])
 # Remove any existent events.
 events = client.events().list(calendarId=calendar["id"]).execute()["items"]
 if len(events) > 0:
-    print(f"Reseting calendar. Removing {len(events)} events found.")
+    print(f"Reseting calendar, removing {len(events)} events found")
+
+    batch = client.new_batch_http_request()
     for event in events:
-        client.events().delete(calendarId=calendar["id"], eventId=event["id"]).execute()
+        batch.add(client.events().delete(calendarId=calendar["id"], eventId=event["id"], sendUpdates="all"))
+        
+    batch.execute()
 
 # Search and read for TOML files. Aggregate the ones that contains `slot` configuration.
 slots = []
 conferences = Path(config("CONFERENCES_PATH", "conferencias"))
 for config_file in conferences.glob("**/*.toml"):
+    print("Reading config file:", config_file)
     with config_file.open() as fp:
         config = toml.load(fp)
         slots.extend(config.get("slot", []))
 
 # Create Calendar Event for each slot found on configuration files
-print("Total events found in the configuration files:", len(slots))
+print("Total events found:", len(slots))
+batch = client.new_batch_http_request()
 for slot in slots:
     start_at = slot["start_at"]
     if not start_at.year == today.year:
@@ -75,6 +82,9 @@ for slot in slots:
         },
     }
     print("Creating event:", event["summary"])
-    client.events().insert(calendarId=calendar["id"], body=event).execute()
+    batch.add(client.events().insert(calendarId=calendar["id"], body=event))
+
+batch.execute()
 
 print("Calendar ID:", calendar["id"])
+print(f"Calendar UI: https://calendar.google.com/calendar/embed?src={calendar['id']}")
